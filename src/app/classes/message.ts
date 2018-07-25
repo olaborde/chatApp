3a58d85cb0276c019c75947d1fcfd90e3a153a9e
olaborde <oml007@live.com>
2018-07-24 20:46:32 -0400

    setup message properties

--- src/app/classes/message.ts
+++ src/app/classes/message.ts
@@ -1,2 +1,15 @@
+import { User } from './user';
+import { send } from '../../../node_modules/@types/q';
+
 export class Message {
+  message: string;
+  createdAt: Date;
+  sender: User;
+
+  constructor({message, createdAt, sender}) {
+    this.message = message;
+    this.createdAt = createdAt;
+    this.sender = sender;
+
+  }
 }
