d0babd27cc55757aa6a1da11889fff2af5489516
olaborde <oml007@live.com>
2018-07-27 23:52:31 -0400

    dummy data sample

--- src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
+++ src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatroom-window.component.scss']
 })
 export class ChatroomWindowComponent implements OnInit {
-  public dummyData = [ ];
+  public dummyData = [
+    {
+      message: 'la lal alala',
+      createdAt: new Date(),
+      sender: {
+        firstName: 'steve',
+        lastName: 'Smith',
+        photoUrl: 'http://via.placeholder.com/150x150'
+      }
+    }
+   ];
   constructor() { }
 
   ngOnInit() {
